e6401215d57757b75699ccc1fd98ca279a593a78
Anushree <anushree@Anushree-iMac.local>
2022-02-28 10:02:06 +0530

    weather app

---
+++ WeatherApp/WeatherApp/WeatherViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Anushree on 24/02/22.
+//
+
+//import Foundation
+//
+//class WeatherViewModel {
+//
+//    var networkManager: NetworkManager
+//    var cityName: String = "City Name"
+//    var description: String = ""
+//
+//    init(networkManager: NetworkManager) {
+//        self.networkManager = networkManager
+//    }
+//
+//    func refresh() {
+//        networkManager.loadWeatherData { Weather in
+//            self.cityName = Weather.city
+//            self.description = Weather.description
+//        }
+//    }
+//}
+
